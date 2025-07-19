f16eee7b4ccdd765661a109dee7767d9602f8ef7
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-19 01:37:26 -0500

    Testing no footer

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.querySelector('header').innerHTML = `
 `;
 
 /*** FOOTER ***/
-document.querySelector('footer').innerHTML = `
-    <small id="copyright" class="center">&copy; 2025 Adriana Zambrano. All rights reserved.</small>
-`;
+// Saving for later
+// document.querySelector('footer').innerHTML = `
+//     <small id="copyright" class="center">&copy; 2025 Adriana Zambrano. All rights reserved.</small>
+// `;
