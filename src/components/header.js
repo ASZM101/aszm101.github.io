7de2b5c43ce701eb986eb279b609301d78e5fb79
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-18 21:05:48 -0500

    Refactored nav logo to start nav bar

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,5 @@
 document.querySelector('header').innerHTML = `
-    <a href="index.html"><img src="assets/img/logo.png" alt="Logo" title="Home" id="nav-logo"></a>
+    <nav>
+        <a href="index.html"><img src="assets/img/logo.png" alt="Logo" title="Home" id="nav-logo"></a>
+    </nav>
 `;
