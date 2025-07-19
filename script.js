18454838ff2516a413f4110013eb5c958a612777
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-19 02:17:25 -0500

    Testing out Medium widget

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.querySelector('header').innerHTML = `
     <a href="index.html" id="nav-logo"><img src="assets/img/logo.png" alt="Logo" title="Home"></a>
     <nav>
         <a href="projects.html"><img src="assets/img/projects.png" alt="Light bulb" title="Projects"></a>
-        <a href="https://medium.com/@aszm" target="_blank"><img src="assets/img/articles.png" alt="Paper and pencil" title="Articles"></a>
-        <a href="https://www.youtube.com/@aszm" target="_blank"><img src="assets/img/videos.png" alt="Video camera" title="Videos"></a>
+        <a href="articles.html"><img src="assets/img/articles.png" alt="Paper and pencil" title="Articles"></a>
+        <a href="videos.html"><img src="assets/img/videos.png" alt="Video camera" title="Videos"></a>
         <a href="contact.html"><img src="assets/img/contact.png" alt="Open envelope" title="Contact"></a>
         <a href="resume.html"><img src="assets/img/resume.png" alt="Briefcase" title="Resume"></a>
     </nav>
