263cb025afbe658c3247b9480897e934bba0fcbf
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-19 01:06:53 -0500

    Embedded resume within page with nav bar

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.querySelector('header').innerHTML = `
         <a href="https://medium.com/@aszm" target="_blank"><img src="assets/img/articles.png" alt="Paper and pencil" title="Articles"></a>
         <a href="https://www.youtube.com/@aszm" target="_blank"><img src="assets/img/videos.png" alt="Video camera" title="Videos"></a>
         <a href="contact.html"><img src="assets/img/contact.png" alt="Open envelope" title="Contact"></a>
-        <a href="resume.pdf" target="_blank"><img src="assets/img/resume.png" alt="Briefcase" title="Resume"></a>
+        <a href="resume.html"><img src="assets/img/resume.png" alt="Briefcase" title="Resume"></a>
     </nav>
 `;
 
