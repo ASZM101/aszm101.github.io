bb09da96757477dc1fc3eaf7950d8de720c4d628
Adriana Zambrano <adrianasofia.zambrano@gmail.com>
2025-07-27 01:09:26 -0500

    Created new header and Discord username copied alert in Spanish

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 /*** HEADER ***/
+
+// Header in English
 document.querySelector('header.en').innerHTML = `
     <a href="index.html" id="nav-logo"><img src="assets/img/logo-shadow.png" alt="Logo" title="Home"></a>
     <nav>
@@ -7,6 +9,18 @@ document.querySelector('header.en').innerHTML = `
         <a href="videos.html"><img src="assets/img/videos.png" alt="Video camera" title="Videos"></a>
         <a href="contact.html"><img src="assets/img/contact.png" alt="Open envelope" title="Contact"></a>
         <a href="resume.html"><img src="assets/img/resume.png" alt="Briefcase" title="Resume"></a>
+    </nav>
+`;
+
+// Header in Spanish (file paths relative to desitnation HTML page)
+document.querySelector('header.es').innerHTML = `
+    <a href="inicio.html" id="nav-logo"><img src="../assets/img/logo-shadow.png" alt="Logo" title="Inicio"></a>
+    <nav>
+        <a href="proyectos.html"><img src="../assets/img/projects.png" alt="Bombillo" title="Proyectos"></a>
+        <a href="articulos.html"><img src="../assets/img/articles.png" alt="Papel y lápiz" title="Artículos"></a>
+        <a href="videos.html"><img src="../assets/img/videos.png" alt="Cámara de video" title="Videos"></a>
+        <a href="contacto.html"><img src="../assets/img/contact.png" alt="Sobre abierto" title="Contacto"></a>
+        <a href="curriculum.html"><img src="../assets/img/resume.png" alt="Maletín" title="Currículum"></a>
     </nav>
 `;
 
@@ -59,7 +73,7 @@ contactFormIcon.addEventListener('mouseover', () => {
     name = oldFile.slice(0, oldFile.indexOf('-'));
     contactFormIcon.src = `assets/img/${name}-black.png`;
     // While mouseover (on hover): icon is black
-})
+});
 contactFormIcon.addEventListener('mouseout', () => {
     // Before mouseout (on hover): icon is black
     fullSrc = contactFormIcon.src.split('/');
@@ -67,15 +81,24 @@ contactFormIcon.addEventListener('mouseout', () => {
     name = oldFile.slice(0, oldFile.indexOf('-'));
     contactFormIcon.src = `assets/img/${name}-white.png`;
     // While mouseout (original): icon is white
-})
+});
 
 /*** COPY BUTTONS ***/
 
-// Discord username
+// Discord username in English
 document.querySelector('button.discord.en').addEventListener('click', (e) => {
     const username = document.querySelector('#discord-username');
     username.select();
     navigator.clipboard.writeText(username.value);
     document.execCommand('copy');
     alert(`Copied Discord username: ${username.value}`);
-})
+});
+
+// Discord username in Spanish
+document.querySelector('button.discord.es').addEventListener('click', (e) => {
+    const username = document.querySelector('#discord-username');
+    username.select();
+    navigator.clipboard.writeText(username.value);
+    document.execCommand('copy');
+    alert(`Usuario de Discord copiado: ${username.value}`);
+});
